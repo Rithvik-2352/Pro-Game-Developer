37790b14e72b95b261d93f9e70d301f0228a0a75
Rithvik-2352 <rithvikdilip@outlook.com>
2026-08-05 19:46:32 -0700

    Images

---
+++ images.py
@@ -0,0 +1,39 @@
+import pygame
+pygame.init ()
+W = 1000
+H = 500
+screen = pygame.display.set_mode ((W,H))
+background = pygame.image.load ("images/beach.jpg")
+background = pygame.transform.scale (background,(1000,500))
+bird = pygame.image.load ("images/bird.png")
+bird = pygame.transform.scale (bird,(50,50))
+class character (pygame.sprite.Sprite):
+    def __init__ (self, x, y):
+        super().__init__()
+        self.x = x
+        self.y = y
+        self.image = bird
+        self.rect = self.image.get_rect ()
+        self.rect.topleft = (self.x,self.y)
+    def update (self, key):
+        if key [pygame.K_UP]:
+            self.rect.y -= 5
+        if key [pygame.K_DOWN]:
+            self.rect.y += 5    
+bird1 = character (500,250)
+birdGroup = pygame.sprite.Group () 
+birdGroup.add (bird1)       
+while (True):
+    for event in pygame.event.get ():
+        if event.type == pygame.QUIT:
+            pygame.quit ()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                bird1.rect.x += 10
+            if event.key == pygame.K_LEFT:
+                bird1.rect.x -= 10
+    screen.blit (background,(0,0))
+    key = pygame.key.get_pressed ()
+    birdGroup.update (key)
+    birdGroup.draw (screen)
+    pygame.display.update ()
