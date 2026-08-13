149b045ec020de52c955e81bb4944dfe5919bd28
Rithvik-2352 <rithvikdilip@outlook.com>
2026-08-12 19:24:27 -0700

    Finished Images.py file and added a new function to process images.

--- images.py
+++ images.py
@@ -1,4 +1,5 @@
 import pygame
+import time
 pygame.init ()
 W = 1000
 H = 500
@@ -17,13 +18,16 @@ class character (pygame.sprite.Sprite):
         self.rect.topleft = (self.x,self.y)
     def update (self, key):
         if key [pygame.K_UP]:
-            self.rect.y -= 5
+            self.rect.y -= 1
         if key [pygame.K_DOWN]:
-            self.rect.y += 5    
+            self.rect.y += 1  
+        print (self.rect.y)
 bird1 = character (500,250)
 birdGroup = pygame.sprite.Group () 
-birdGroup.add (bird1)       
+birdGroup.add (bird1)
+clock = pygame.time.Clock ()       
 while (True):
+    clock.tick(60)
     for event in pygame.event.get ():
         if event.type == pygame.QUIT:
             pygame.quit ()
@@ -32,7 +36,12 @@ while (True):
                 bird1.rect.x += 10
             if event.key == pygame.K_LEFT:
                 bird1.rect.x -= 10
+            if event.key == pygame.K_SPACE:
+                bird1.rect.y -= 40
+                
+
     screen.blit (background,(0,0))
+    bird1.rect.y +=0.5
     key = pygame.key.get_pressed ()
     birdGroup.update (key)
     birdGroup.draw (screen)
